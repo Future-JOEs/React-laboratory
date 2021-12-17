42182abb1eb263022a3f4b81fdd3cb6455dd38e7
qiaoyongjian <qiaoyongjian@360.cn>
2021-12-17 19:07:28 +0800

    feat: npm run eject

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="box"></div>
-      <div className="sector"></div>
+    <div>
+      <div className="container">
+        <div className="box1">
+          <div className="sector"></div>
+        </div>
+        <div className="box2">
+          <div className="box"></div>
+        </div>
+        <div className="box3"></div>
+      </div>
+      <div className="container1">
+        <div className="left">1</div>
+        <div className="right">3</div>
+        <div className="content">2</div>
+      </div>
     </div>
   );
 }
