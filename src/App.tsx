f219f88fb96da0e6da5030db78a5bbfe5dca0f63
qiaoyongjian <qiaoyongjian@360.cn>
2021-12-17 19:32:28 +0800

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,25 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const handleJsMove = () => {
+    let e = document.getElementById("js_move");
+    animate(e, 100);
+  };
+
+  const animate = (ele: any, spd: any) => {
+    var start = Date.now(); // remember start time
+    var timer = setInterval(function () {
+      var timePassed = Date.now() - start;
+      var step = Math.ceil(Math.abs(timePassed - 5000) / 10);
+      console.log(step);
+      if (timePassed >= 5000) {
+        clearInterval(timer); // finish the animation after 2 seconds
+        return;
+      }
+      ele.style.left = ele.offsetLeft + step + "px";
+    }, spd);
+  };
+
   return (
     <div>
       <div className="container">
@@ -17,6 +36,10 @@ function App() {
         <div className="left">1</div>
         <div className="right">3</div>
         <div className="content">2</div>
+      </div>
+      <div className="container2">
+        <div className="css_move"></div>
+        <div id="js_move" onClick={handleJsMove} className="js_move"></div>
       </div>
     </div>
   );
